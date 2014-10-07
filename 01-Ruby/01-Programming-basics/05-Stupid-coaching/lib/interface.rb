9e653fae225adfbd990acdbf79e17282b7092bb8
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-07 17:21:23 +0200

    exo5

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,12 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+
+
+puts "Hello, a toi de parler"
+
+my_message = gets.chomp
+
+until my_message == "I am going to work right now!" || my_message == "I AM GOING TO WORK RIGHT NOW!"
+puts coach_answer_enhanced(my_message)
+my_message = gets.chomp
+end
