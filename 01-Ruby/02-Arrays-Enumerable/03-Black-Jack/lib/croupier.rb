45a7976e39549fc3a6c99203d975ee10f394f46f
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-08 18:49:29 +0200

    revu

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -8,13 +8,13 @@ end
 def end_game_message(player_score, bank_score)
   # TODO: return (not print!) a message telling if the user won or lost.
   if player_score == 21
-  return "Black Jack"
+    return "Black Jack"
   elsif player_score > 21
-  return "You lose"
+    return "You lose"
   elsif player_score > bank_score
-  return "You win"
+    return "You win"
   else
-  return "You lose"
+    return "You lose"
   end
 end
 
@@ -23,11 +23,10 @@ def play_game
   #       when the user will not be asking for  a new card
   player_score = pick_player_card
   bank_score = pick_bank_score
-
   puts "Card ? (type 'y' or 'yes' for a new card)"
   saisie = gets.chomp
 
-  while saisie == "yes" || saisie == "y" && player_score <=21
+  while saisie == "yes" || saisie == "y" && player_score <= 21
     state_of_the_game
     puts "Card ? (type 'y' or 'yes' for a new card)"
     saisie = gets.chomp
