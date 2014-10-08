39250967684509706448df35ac977b2a5852ff03
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-08 18:42:02 +0200

    back jack fini

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -2,14 +2,36 @@ require_relative 'black_jack'
 
 def state_of_the_game(player_score, bank_score)
   # TODO: return (not print!) a message containing the player's score and bank's score
+  "Your score is #{player_score}, bank is #{bank_score}!"
 end
 
 def end_game_message(player_score, bank_score)
   # TODO: return (not print!) a message telling if the user won or lost.
+  if player_score == 21
+  return "Black Jack"
+  elsif player_score > 21
+  return "You lose"
+  elsif player_score > bank_score
+  return "You win"
+  else
+  return "You lose"
+  end
 end
-
 
 def play_game
   # TODO: make the user play from terminal in a while loop that will stop
   #       when the user will not be asking for  a new card
+  player_score = pick_player_card
+  bank_score = pick_bank_score
+
+  puts "Card ? (type 'y' or 'yes' for a new card)"
+  saisie = gets.chomp
+
+  while saisie == "yes" || saisie == "y" && player_score <=21
+    state_of_the_game
+    puts "Card ? (type 'y' or 'yes' for a new card)"
+    saisie = gets.chomp
+    player_score = player_score + pick_player_card
+  end
+    puts end_game_message(player_score, bank_score)
 end
--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/run_game.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/run_game.rb
@@ -1,3 +1,4 @@
 require_relative "croupier.rb"
 
 play_game
+
