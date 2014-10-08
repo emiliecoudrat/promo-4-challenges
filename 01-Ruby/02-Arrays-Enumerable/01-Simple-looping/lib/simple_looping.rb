a61c47bd7999303fe128f046d414b40f9b5f6aac
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-08 12:48:15 +0200

    exo 1

--- 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
@@ -1,12 +1,26 @@
+
 def sum_with_while(min, max)
   # CONSTRAINT: you should use a while..end structure
+  sum = min
+  min = min +1
+  while min <= max
+    sum = sum + min
+    min += 1
+  end
+  sum
 end
+
 
 def sum_with_for(min, max)
   # CONSTRAINT: you should use a for..end structure
+  sum = 0
+  for num in min..max
+    sum = sum + num
+  end
+  sum
 end
+
 
 def sum_recursive(min, max)
   # CONSTRAINT: you should use a recursive algorithm, i.e. the method should call itself
 end
-
