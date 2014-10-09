93cb3a2d35ca9b31ebce43da7bd942c7d86f8db4
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-09 19:12:06 +0200

    pas fini

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -2,4 +2,24 @@
 
 def louchebemize(sentence)
   # TODO: implement your louchebem translator
+
+# je veux transformer boucher en loucherbem
+
+
+# je prends les connsones du début du mot
+  suffixe = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+
+  if word[0].include? ["a", "e", "i", "o", "u", "y"]
+    return "l#{word_french}#{suffixe}#"
+  elsif word.length = 1
+  elsif word
+  end
+
+#la consonne ou le groupe de consonnes au début du mot est reportée à la fin du mot et remplacée par un "l"
+
+# sac => lacsé
+# boucher => loucherbem
+# jargon => largonji
+
+
 end
