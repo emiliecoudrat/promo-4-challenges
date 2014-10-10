2ab5cf2eb550bc1e32fe5691ca08952b2c2eb135
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-10 17:59:12 +0200

    ok

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,14 @@
+
+
 def array_to_hash(array)
   #TODO: implement the method :)
+  hash = Hash.new
+  array.each_with_index { |item, index|
+
+  hash[block_given? ? yield(index) : index] = item
+  }
+return hash
 end
+
+
+puts array_to_hash(["emilie","rat"]) { |x| x = "key#{x+1}" }
