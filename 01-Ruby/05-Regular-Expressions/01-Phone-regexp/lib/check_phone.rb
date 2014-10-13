5dd45f34a072322b804f347434f414b852e98eb6
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-13 15:32:49 +0200

    ok

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,11 @@
+
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  if phone_number =~ /^(0|\+33)[\s-]?[167][\s-]?(\d{2}[\s-]?){4}$/
+    return true
+  else
+    return false
+  end
 end
+
+puts french_phone_number?("06 65 36 36 36")
