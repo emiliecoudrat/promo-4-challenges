a76a23e6fd7f925e4f468a3089224a93bf2b1cb5
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-14 19:01:16 +0200

    pas ok

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
@@ -3,8 +3,30 @@ require 'json'
 
 def generate_grid(grid_size)
   # TODO: generate random grid of letters
+
+chars = 'ABCDEFGHJKLMNOPQRSTUVWXYZ'
+Array.new(grid_size) { chars[rand(chars.length)].chr }
+
+# array = []
+# grid_size.times { array << [*("A".."Z")].sample }
+# return array
 end
+
+# def time(start_time,end_time)
+#   start_time = Time.now
+#   yield
+#   end_time = Time.now
+#   counter = (end_time - start_time).to_f
+#   return counter
+# end
+
 
 def run_game(attempt, grid, start_time, end_time)
   # TODO: runs the game and return detailed hash of result
+  counter = (end_time - start_time).to_f
+  return counter
 end
+
+# time do
+#   gets.chomp(attempt)
+# end
