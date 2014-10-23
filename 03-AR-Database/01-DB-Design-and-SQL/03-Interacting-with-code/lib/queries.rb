7b5383d8b259320e762ad7dce4cc2683fdfc58a2
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-23 14:55:17 +0200

    2/5

--- 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
@@ -2,6 +2,8 @@ require 'sqlite3'
 
 def number_of_rows(db, table_name)
   # TODO: count number of rows in table table_name
+  rows = db.execute "SELECT COUNT(*) FROM #{table_name}"
+  return rows.join.to_i
 end
 
 def sorted_artists(db)
