a95d663218d09e4b2ff778e600e4056fcb9f0217
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-23 16:52:49 +0200

    1/3

--- 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
@@ -2,6 +2,10 @@ require 'sqlite3'
 
 def detailed_tracks(db)
   # TODO: return the list of tracks with their album and artist
+  tab = db.execute"SELECT t.name, al.title, ar.name FROM tracks t
+   JOIN albums al ON (t.album_id = al.id)
+   JOIN artists ar ON (al.artist_id = ar.id);"
+  tab.map!(&:flatten)
 end
 
 def stats_on(db, genre_name)
@@ -11,3 +15,7 @@ end
 def top_five_artists(db, genre_name)
   # TODO: return list of top 5 rock artists
 end
+
+# SELECT *
+# FROM A
+# INNER JOIN B ON A.key = B.key
