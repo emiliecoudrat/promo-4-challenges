3fdd6f0888384b0ad414cbd81f051deefbd26b7d
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-27 16:37:25 +0100

    ok

--- 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/controllers/posts_controller.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 require_relative "../views/post_view.rb"
+require_relative "../models/post.rb"
 
 class PostsController
   def initialize
@@ -9,25 +10,43 @@ class PostsController
     # DO NOT WRITE SQL QUERIES
     # TODO: gather all posts from the database
     # TODO: give them to the view to be printed
+    post = Post.all
+    @view.display_posts(post)
   end
 
   def create
     # DO NOT WRITE SQL QUERIES
     # TODO: create a post
+    attributes = @view.ask_for_name_url_vote
+    new_post = Post.new(attributes)
+    new_post.save
   end
 
   def update
     # DO NOT WRITE SQL QUERIES
     # TODO: update a post
+    search_id, new_attributes = @view.update_name_url
+    find_post = Post.find(search_id)
+    find_post.name = new_attributes[:name]
+    find_post.url = new_attributes[:url]
+    find_post.save
   end
 
   def destroy
     # DO NOT WRITE SQL QUERIES
     # TODO: destroy a post
+    id_to_delete = @view.post_to_delete
+    find_post = Post.find(id_to_delete)
+    find_post.destroy
   end
 
   def upvote
     # DO NOT WRITE SQL QUERIES
     # TODO: increment the `votes` column for a post
+    id = @view.add_vote
+    post_vote = Post.find(id)
+    post_vote.votes += 1
+    post_vote.save
+    # find_post = Post.find(id_to_vote)
   end
 end
--- 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/views/post_view.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Models/app/views/post_view.rb
@@ -1,3 +1,56 @@
 class PostView
   # TODO: implement some methods here when controller needs to `puts` or `gets`
+  def display_posts(posts)
+    puts "Voici la liste de tous les postes :"
+    posts.each do |post|
+      puts "#{post.id} - #{post.name} #{post.url} #{post.votes}"
+    end
+  end
+
+  def ask_for_name_url_vote
+    attributes = {}
+    puts "please enter a name"
+    print ">"
+    attributes[:name] = gets.chomp
+    puts "please enter a url"
+    print ">"
+    attributes[:url] = gets.chomp
+    puts "please vote"
+    print ">"
+    attributes[:votes] = gets.chomp
+    return attributes
+  end
+
+  def update_name_url
+    new_attributes = {}
+    puts "Enter the post id you want to edit"
+    print ">"
+      begin
+        search_id = gets.chomp.to_i
+      rescue ArgumentError
+        puts "Cet id n'existe pas"
+      end
+    puts "Edit name"
+    print ">"
+    new_attributes[:name] = gets.chomp
+    puts "Edit url"
+    print ">"
+    new_attributes[:url] = gets.chomp
+    return search_id, new_attributes
+  end
+
+  def post_to_delete
+    puts "Enter the post id you want to delete"
+    print ">"
+    id_to_delete = gets.chomp.to_i
+    return id_to_delete
+  end
+
+  def add_vote
+    puts "Enter the post id you want to vote for"
+    print ">"
+    id = gets.chomp.to_i
+    return id
+  end
+
 end
