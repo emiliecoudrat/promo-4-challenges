9e6ea83da3e0d55f00774832cf8a171cce23c03f
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-28 11:23:52 +0100

    ok

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/migrate/20141025152200_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration
       t.string :name
       t.string :url
       t.integer :votes
+      t.references :user
       t.timestamps
     end
   end
--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,18 @@
 # TODO: Write a seed
+require 'faker'
+
+5.times do
+  user = User.create ({
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.email
+  })
+
+
+  (5..10).to_a.sample.times do
+    user.posts.create ({
+      name: Faker::Lorem.word,
+      url: Faker::Internet.url
+    })
+  end
+end
+
