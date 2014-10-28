d87b06b14340c4a126a11a635403389e68c678bb
emiliecoudrat <coudrat.emilie@gmail.com>
2014-10-28 12:14:49 +0100

    pas fini

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   # TODO: Add some validation
+  validates_presence_of :user
+  validates_presence_of :name
+  validates :name, length: { minimum: 5 }
+  validates :name, uniqueness: {case_sensitive: false}
+  validates_presence_of :url, format: { with: /\A\.com\z/ }
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
 
   # TODO: Add some validation
+  validates_presence_of :username
+  validates_uniqueness_of :username
+  validates_presence_of :email, format: { with: /\s*\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*\s*/}
 end
